4a9495eda7786a61a4951f22054ca70c6bbf4bd2
Keshav Melnad <keshav.s.m@gmail.com>
2025-06-26 13:17:47 +0300

    fixed sidebar theme

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -56,7 +56,7 @@ export default async function RootLayout({
     <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
       <div className="flex flex-col flex-1">
-        <header className="bg-sidebar text-sidebar-foreground border-b shadow-xs flex justify-between items-center h-16 shrink-0 gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="bg-sidebar  border-b shadow-xs flex justify-between items-center h-16 shrink-0 gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
